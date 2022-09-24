aa13165adf1fb2ee7565949498939d123e7e6a0a
hujun <18660093805@163.com>
2022-09-24 17:02:19 +0800

    修改docker容器时区

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t yoyo-push-image .
-docker run -d --name yoyo-push yoyo-push-image:latest
+docker run -d --name yoyo-push -e TZ="Asia/Shanghai" yoyo-push-image:latest
 docker update yoyo-push --restart=always
